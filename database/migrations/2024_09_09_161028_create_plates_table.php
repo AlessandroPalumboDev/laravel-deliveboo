b2113092e4500f857303ed2dd827f4d1203f58d5
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-09-09 18:12:33 +0200

    [UPD] Plate table created

---
+++ database/migrations/2024_09_09_161028_create_plates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 50);
+            $table->decimal('price', $precision = 4, $scale = 2);
+            $table->string('cover_image', 255);
+            $table->text('description')->nullable();
+            $table->text('ingredients', 255);
+            $table->boolean('is_visible');
+            $table->boolean('is_vegetarian');
+            $table->boolean('is_vegan');
+            $table->boolean('is_gluten_free');
+            $table->boolean('is_lactose_free');
+            $table->boolean('is_spicy');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plates');
+    }
+};
