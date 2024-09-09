df6b113d97520fdb143b7daa6d758912df73cbf4
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-09-09 18:38:56 +0200

    [UPD] Foreign key added

---
+++ database/migrations/2024_09_09_163358_add_restaurant_id_foreign_to_plates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('plates', function (Blueprint $table) {
+            $table->unsignedBigInteger('restaurant_id')->nullable()->after('id');
+
+            //crea la chiave esterna
+            $table->foreign('restaurant_id')
+                ->references('id')
+                ->on('plates')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('plates', function (Blueprint $table) {
+
+            //droppiamo la relazione tra le tabelle
+            $table->dropForeign('plates_restaurant_id_foreign');
+            //droppiamo il campo
+            $table->dropColumn('restaurant_id');
+            
+        });
+    }
+};
