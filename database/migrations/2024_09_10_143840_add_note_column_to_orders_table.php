f3e7e21ce7d5a36796260cf9c286355c795fda93
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-09-10 16:40:08 +0200

    [UPD] Add note column to orders table

---
+++ database/migrations/2024_09_10_143840_add_note_column_to_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // Aggiunge la colonna note alla tabella orders
+            $table->text('note')->nullable()->after('order_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // Rimuove la colonna note
+            $table->dropColumn('note');
+        });
+    }
+};
