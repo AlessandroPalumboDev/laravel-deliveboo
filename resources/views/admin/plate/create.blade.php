6d2c6d0f381b079e19489f42823d5fcd9aefab84
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-09-11 19:30:28 +0200

    [FIX] Valori booleani nel form di creazione di un piatto

--- resources/views/admin/plate/create.blade.php
+++ resources/views/admin/plate/create.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <div class="card">
+                <div class="card mt-5">
                     <div class="card-header">
                         <h1 class="text-center">
                             crea il tuo piatto
@@ -34,63 +34,70 @@
                                     value="{{ old('ingredients') }}" />
                             </div>
 
-                            <div class="mb-3">
+                            <div class="mb-4">
                                 <label for="price" class="form-label">Prezzo del piatto</label>
                                 <input type="number" class="form-control" id="price" name="price"
                                     value="{{ old('price') }}" required />
 
                             </div>
 
-                            <div class="mb-3">
-                                <input class="form-check-input" type="checkbox" name="is_vegetarian" id="is_vegetarian"
-                                    value="1" {{ old('is_vegetarian') ? 'checked' : '' }}>
-                                <label class="form-check-label" for="is_vegetarian">È vegetariano?</label>
-                            </div>
-                            <div class="mb-3">
-                                <label for="is_vegan " class="form-label"></label>
-                                <input class="form-check-input" type="checkbox" name="is_vegan "
-                                    @if ('checked') value={{ 'is_vegan' }} @endif id="is_vegan ">
-                                <label class="form-check-label" for="is_vegan ">è vegano? </label>
+                            <div class="d-flex gap-4 mb-3">
+                                <div class="mb-3">
+                                    <input class="form-check-input" type="checkbox" name="is_vegetarian" id="is_vegetarian"
+                                        value="1" {{ old('is_vegetarian') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="is_vegetarian">Vegetariano</label>
+                                </div>
+    
+                                <div class="mb-3">
+                                    <input class="form-check-input" type="checkbox" name="is_vegan"
+                                        value="1" {{ old('is_vegan') ? 'checked' : '' }} id="is_vegan">
+                                    <label class="form-check-label" for="is_vegan">Vegano</label>
+                                </div>
+    
+                                <div class="mb-3">
+                                    <input class="form-check-input" type="checkbox" name="is_gluten_free"
+                                        value="1" {{ old('is_gluten_free') ? 'checked' : '' }} id="is_gluten_free">
+                                    <label class="form-check-label" for="is_gluten_free">Senza Glutine</label>
+                                </div>
+    
+                                <div class="mb-3">
+                                    <input class="form-check-input" type="checkbox" name="is_lactose_free"
+                                        value="1" {{ old('is_lactose_free') ? 'checked' : '' }} id="is_lactose_free">
+                                    <label class="form-check-label" for="is_lactose_free">Senza Lattosio</label>
+                                </div>
+    
+                                <div class="mb-3">
+                                    <input class="form-check-input" type="checkbox" name="is_spicy"
+                                        value="1" {{ old('is_spicy') ? 'checked' : '' }} id="is_spicy">
+                                    <label class="form-check-label" for="is_spicy">Piccante</label>
+                                </div>
+    
+                                <div class="mb-3">
+                                    <input class="form-check-input" type="checkbox" name="is_visible"
+                                        value="1" {{ old('is_visible') ? 'checked' : '' }} id="is_visible">
+                                    <label class="form-check-label" for="is_visible">Visibile</label>
+                                </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label for="is_gluten_free" class="form-label"></label>
-                                <input class="form-check-input" type="checkbox" name="is_gluten_free"
-                                    @if ('checked') value={{ 'is_gluten_free' }} @endif
-                                    id="is_gluten_free">
-                                <label class="form-check-label" for="is_gluten_free">è senza glutine? </label>
-                            </div>
+                            
 
+                            
                             <div class="mb-3">
-                                <label for="is_lactose_free" class="form-label"></label>
-                                <input class="form-check-input" type="checkbox" name="is_lactose_free"
-                                    @if ('checked') value={{ 'is_lactose_free' }} @endif
-                                    id="is_lactose_free">
-                                <label class="form-check-label" for="is_lactose_free">è senza lattosio? </label>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="is_spicy" class="form-label"></label>
-                                <input class="form-check-input" type="checkbox" name="is_spicy" id="is_spicy"
-                                    @if ('checked') value={{ 'is_spicy' }} @endif>
-                                <label class="form-check-label" for="is_spicy">è piccante? </label>
+                                <label for="cover_image" class="form-label">Immagine del Piatto</label>
+                                <input class="form-control" type="file" id="cover_image" name="cover_image"
+                                    value="{{ old('image_path') }}">
+            
                             </div>
                         </div>
 
-                </div>
-                <div class="mb-3">
-                    <label for="cover_image" class="form-label">Image</label>
-                    <input class="form-control" type="file" id="cover_image" name="cover_image"
-                        value="{{ old('image_path') }}">
-
-                </div>
-
-                <div class="card-footer">
-                    <a href="{{ route('admin.Restaurants.store') }}">
-                        <button class="btn btn-outline-primary">
-                            Crea
-                        </button>
-                    </a>
+        
+                        <div class="card-footer">
+                            <a href="{{ route('admin.Restaurants.store') }}">
+                                <button class="btn btn-outline-primary">
+                                    Crea
+                                </button>
+                            </a>
+                        </div>
                 </div>
 
 
