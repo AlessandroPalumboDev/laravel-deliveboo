666ea1e2977e2ea9c1cce58b5a751b2a4630a977
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-09-11 20:02:28 +0200

    [FIX] Piccola aggiustatina ma non definitiva a index.blade dei piatti

--- resources/views/admin/plate/index.blade.php
+++ resources/views/admin/plate/index.blade.php
@@ -11,32 +11,45 @@
                         @if ($plates && $plates->count() > 0)
                             @foreach ($plates as $plate)
                                 <div class="row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <div class="col-sm-6 mb-3 mb-sm-0 ">
                                         <div class="card">
+                                            <div class="card-header">
+                                                <h4 class="card-title">{{ $plate->name }}</h4>
+                                            </div>
                                             <div class="card-body">
-                                                <h5 class="card-title">Special title treatment</h5>
-                                                <p class="card-text">{{ $plate->name }}</p>
-
+{{-- 
+                                                <div class="card-img">
+                                                    <img :src="{{$plate->cover_image}}" :alt="{{ $plate->name }}">
+                                                </div> --}}
+                                                
+                                                <div class="card-text mb-3">
+                                                    <h6>Descrizione:</h6>
+                                                    <p>{{ $plate->description }}</p>
+                                                </div>
+                                                <div class="card-text mb-3">
+                                                    <h6>Ingredienti:</h6>
+                                                    <p>{{ $plate->ingredients }}</p>
+                                                </div>
+                                                
                                             </div>
                                         </div>
                                     </div>
                             @endforeach
-                            <a href="{{ route('admin.plates.create') }}" class="btn btn-outline-primary">aggiungi
-                                un piatto</a>
-                        @else
-                            <div class="alert alert-danger" role="alert">
-                                <h3>
-                                    Non hai ancora alcun piatto nel menu
-                                </h3>
-                                <a href="{{ route('admin.plates.create') }}" class="btn btn-outline-primary">aggiungi
-                                    un piatto</a>
-                            </div>
-                        @endif
-
-
+                            
+                            
+                        </div>
+                        
                     </div>
-
-                </div>
+                    <a href="{{ route('admin.plates.create') }}" class="btn btn-outline-primary">aggiungi un piatto</a>
+                @else
+                    <div class="alert alert-danger" role="alert">
+                        <h3>
+                            Non hai ancora alcun piatto nel menu
+                        </h3>
+                        <a href="{{ route('admin.plates.create') }}" class="btn btn-outline-primary ">aggiungi
+                            un piatto</a>
+                    </div>
+                @endif
 
 
             </div>
