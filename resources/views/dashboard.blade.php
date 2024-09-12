4545e66f432a5af0746e7fe671c1906660e81b3e
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-09-12 20:11:30 +0200

    [FIX] restaurant card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,6 @@
 
                                     <div class="card-body d-flex g-0">
 
-<<<<<<< HEAD
                                         <div class="card-img my-3">
                                             <img src="{{ asset('storage/' . $restaurant->image_path) }}" class="img-fluid rounded-start">
                                         </div> 
@@ -36,14 +35,8 @@
                                                         {{ $user->surname }}</strong></p>
                                                 <p class="card-text">Partita IVA: <strong>{{ $user->p_iva }}</strong> </p>
                                             @endforeach
-                                            
-=======
-                                        <div class="d-flex justify-content-around py-2">
-                                            <a href={{ route('admin.plates.index') }} class="btn btn-outline-primary">My
-                                                Foods</a>
                                             <a href={{ route('admin.Restaurants.edit', $restaurant) }}
                                                 class="btn btn-outline-primary">edit restaurant</a>
-                                            <a href="#" class="btn btn-outline-primary">My Orders</a>
                                             <form action="{{ route('admin.Restaurants.destroy', $restaurant->id) }}"
                                                 method="POST"
                                                 onsubmit="return confirm('Sei sicuro di voler eliminare questo ristorante?');">
@@ -51,7 +44,7 @@
                                                 @method('DELETE')
                                                 <button type="submit" class="btn btn-danger">Elimina</button>
                                             </form>
->>>>>>> CRUD-RESTAURANT-sd
+                                            
                                         </div>
                                     </div>
                                     <div class="card-footer bg-primary  d-flex justify-content-between">
