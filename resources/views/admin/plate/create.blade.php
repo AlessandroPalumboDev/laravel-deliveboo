12278c3816f4d5e21f904152cbe8b48dc1dc98d0
federicoalagna <federicoalagna01@gmail.com>
2024-09-13 18:08:45 +0200

    [UPD] view arancione

--- resources/views/admin/plate/create.blade.php
+++ resources/views/admin/plate/create.blade.php
@@ -16,72 +16,75 @@
                         <div class="card-body">
 
                             <div class="mb-3">
-                                <label for="name" class="form-label">Nome del piatto <span class="text-danger">*</span></label>
+                                <label for="name" class="form-label">Nome del piatto <span
+                                        class="text-danger">*</span></label>
                                 <input type="text" class="form-control border-primary" id="name" name="name"
                                     value="{{ old('name') }}" required />
                             </div>
 
                             <div class="mb-3">
                                 <label for="description" class="form-label">Descrizione</label>
-                                <input type="text" class="form-control border-primary" id="description" name="description"
-                                    value="{{ old('description') }}" />
+                                <input type="text" class="form-control border-primary" id="description"
+                                    name="description" value="{{ old('description') }}" />
                             </div>
 
                             <div class="mb-3">
-                                <label for="ingredients" class="form-label">Ingredienti <span class="text-danger">*</span></label>
-                                <input type="text" class="form-control border-primary" id="ingredients" name="ingredients"
-                                    value="{{ old('ingredients') }}" />
+                                <label for="ingredients" class="form-label">Ingredienti <span
+                                        class="text-danger">*</span></label>
+                                <input type="text" class="form-control border-primary" id="ingredients"
+                                    name="ingredients" value="{{ old('ingredients') }}" />
                             </div>
 
                             <div class="mb-4">
-                                <label for="price" class="form-label">Prezzo del piatto <span class="text-danger">*</span></label>
+                                <label for="price" class="form-label">Prezzo del piatto <span
+                                        class="text-danger">*</span></label>
                                 <input type="number" class="form-control border-primary" id="price" name="price"
                                     value="{{ old('price') }}" required step="0.01" min="0" />
                             </div>
 
                             <div class="d-flex gap-4 mb-3 flex-wrap">
                                 <div class="mb-3 form-check">
-                                    <input class="form-check-input border-primary" type="checkbox" name="is_vegetarian" id="is_vegetarian"
-                                        value="1" {{ old('is_vegetarian') ? 'checked' : '' }}>
+                                    <input class="form-check-input border-primary" type="checkbox" name="is_vegetarian"
+                                        id="is_vegetarian" value="1" {{ old('is_vegetarian') ? 'checked' : '' }}>
                                     <label class="form-check-label" for="is_vegetarian">Vegetariano</label>
                                 </div>
 
                                 <div class="mb-3 form-check">
-                                    <input class="form-check-input border-primary" type="checkbox" name="is_vegan" value="1"
-                                        {{ old('is_vegan') ? 'checked' : '' }} id="is_vegan">
+                                    <input class="form-check-input border-primary" type="checkbox" name="is_vegan"
+                                        value="1" {{ old('is_vegan') ? 'checked' : '' }} id="is_vegan">
                                     <label class="form-check-label" for="is_vegan">Vegano</label>
                                 </div>
 
                                 <div class="mb-3 form-check">
-                                    <input class="form-check-input border-primary" type="checkbox" name="is_gluten_free" value="1"
-                                        {{ old('is_gluten_free') ? 'checked' : '' }} id="is_gluten_free">
+                                    <input class="form-check-input border-primary" type="checkbox" name="is_gluten_free"
+                                        value="1" {{ old('is_gluten_free') ? 'checked' : '' }} id="is_gluten_free">
                                     <label class="form-check-label" for="is_gluten_free">Senza Glutine</label>
                                 </div>
 
                                 <div class="mb-3 form-check">
-                                    <input class="form-check-input border-primary" type="checkbox" name="is_lactose_free" value="1"
-                                        {{ old('is_lactose_free') ? 'checked' : '' }} id="is_lactose_free">
+                                    <input class="form-check-input border-primary" type="checkbox" name="is_lactose_free"
+                                        value="1" {{ old('is_lactose_free') ? 'checked' : '' }} id="is_lactose_free">
                                     <label class="form-check-label" for="is_lactose_free">Senza Lattosio</label>
                                 </div>
 
                                 <div class="mb-3 form-check">
-                                    <input class="form-check-input border-primary" type="checkbox" name="is_spicy" value="1"
-                                        {{ old('is_spicy') ? 'checked' : '' }} id="is_spicy">
+                                    <input class="form-check-input border-primary" type="checkbox" name="is_spicy"
+                                        value="1" {{ old('is_spicy') ? 'checked' : '' }} id="is_spicy">
                                     <label class="form-check-label" for="is_spicy">Piccante</label>
                                 </div>
 
                             </div>
                             <div class="mb-3 form-check">
-                                <input class="form-check-input border-primary" type="checkbox" name="is_visible" value="1"
-                                    {{ old('is_visible') ? 'checked' : '' }} id="is_visible">
+                                <input class="form-check-input border-primary" type="checkbox" name="is_visible"
+                                    value="1" {{ old('is_visible') ? 'checked' : '' }} id="is_visible">
                                 <label class="form-check-label" for="is_visible">Visibile</label>
                             </div>
 
                             <div class="mb-3">
                                 <label for="cover_image" class="form-label">Immagine del Piatto</label>
-                                <input class="form-control  border-primary" type="file" id="cover_image" name="cover_image"
-                                    value="{{ old('cover_image') }}">
-    
+                                <input class="form-control  border-primary" type="file" id="cover_image"
+                                    name="cover_image" value="{{ old('cover_image') }}">
+
                             </div>
                         </div>
 
--- resources/views/admin/plate/index.blade.php
+++ resources/views/admin/plate/index.blade.php
@@ -1,84 +1,91 @@
-
-
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
 
-            @if ($plates && $plates->count() > 0)
-            <div class="col d-flex justify-content-around">
-                <a href="{{ route('admin.plates.create') }}" class="btn btn-outline-primary   mb-3">Aggiungi un piatto</a>
-                <a href="{{ route('admin.Restaurants.index') }}" class="btn btn-outline-primary   mb-3">Torna al tuo ristorante</a>
+                @if ($plates && $plates->count() > 0)
+                    <div class="col d-flex justify-content-around">
+                        <a href="{{ route('admin.plates.create') }}" class="btn btn-outline-primary   mb-3">Aggiungi un
+                            piatto</a>
+                        <a href="{{ route('admin.Restaurants.index') }}" class="btn btn-outline-primary   mb-3">Torna al tuo
+                            ristorante</a>
+                    </div>
+                    @foreach ($plates as $plate)
+                        <div class="card mb-4 shadow-lg border-primary rounded">
+
+                            <div class="card-header bg-primary text-white">
+                                <h3 class="mb-0">{{ $plate->name }}</h3>
+                            </div>
+
+                            <div class="card-body d-flex g-0">
+
+                                <div class="card-img my-3">
+                                    <img src="{{ asset('storage/' . $plate->cover_image) }}" class="img-fluid rounded-start"
+                                        style="max-height: 200px;">
+                                </div>
+
+                                <div class="col-md-8 my-3 ms-3">
+                                    <p class="card-text text-muted">Descrizione: {{ $plate->description }}</p>
+                                    <p class="card-text">Ingredienti: <strong>{{ $plate->ingredients }}</strong></p>
+                                    <p class="card-text">Prezzo: <strong>€{{ number_format($plate->price, 2) }}</strong></p>
+                                    <div class="d-flex gap-3">
+                                        <a href="{{ route('admin.plates.edit', $plate->id) }}"
+                                            class="btn btn-outline-primary">Modifica</a>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card-footer bg-primary d-flex justify-content-between">
+                                <a href="{{ route('admin.plates.show', $plate->id) }}"
+                                    class="btn btn-outline-light">Dettagli</a>
+
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#deleteModal{{ $plate->id }}">
+                                    Elimina
+                                </button>
+                            </div>
+                        </div>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="deleteModal{{ $plate->id }}" tabindex="-1"
+                            aria-labelledby="deleteModalLabel{{ $plate->id }}" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content border-danger">
+                                    <div class="modal-header border-danger">
+                                        <h1 class="modal-title fs-5 text-danger" id="deleteModalLabel{{ $plate->id }}">
+                                            Elimina Piatto</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Sei sicuro di voler eliminare il piatto <strong>{{ $plate->name }}</strong>?
+                                            Questa azione sarà irreversibile!</p>
+                                    </div>
+                                    <div class="modal-footer border-danger">
+                                        <button type="button" class="btn btn-outline-primary"
+                                            data-bs-dismiss="modal">Annulla</button>
+                                        <form action="{{ route('admin.plates.destroy', $plate->id) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-outline-danger">Elimina</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="alert alert-warning text-center">
+                        <h4 class="alert-heading">Non hai ancora creato nessun piatto!</h4>
+                        <p>Clicca qui sotto per creare subito un piatto e iniziare a vendere.</p>
+                        <a href="{{ route('admin.plates.create') }}" class="btn btn-primary">Crea un Piatto</a>
+                    </div>
+                @endif
             </div>
-                @foreach ($plates as $plate)
-                    <div class="card mb-4 shadow-lg border-primary rounded">
-
-                        <div class="card-header bg-primary text-white">
-                            <h3 class="mb-0">{{ $plate->name }}</h3>
-                        </div>
-
-                        <div class="card-body d-flex g-0">
-
-                            <div class="card-img my-3">
-                                <img src="{{ asset('storage/' . $plate->cover_image) }}" class="img-fluid rounded-start" style="max-height: 200px;">
-                            </div> 
-
-                            <div class="col-md-8 my-3 ms-3">
-                                <p class="card-text text-muted">Descrizione: {{ $plate->description }}</p>
-                                <p class="card-text">Ingredienti: <strong>{{ $plate->ingredients }}</strong></p>
-                                <p class="card-text">Prezzo: <strong>€{{ number_format($plate->price, 2) }}</strong></p>
-                                <div class="d-flex gap-3">
-                                    <a href="{{ route('admin.plates.edit', $plate->id) }}" class="btn btn-outline-primary">Modifica</a>
-                                   
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="card-footer bg-primary d-flex justify-content-between">
-                            <a href="{{ route('admin.plates.show', $plate->id) }}" class="btn btn-outline-light">Dettagli</a>
-
-                            <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $plate->id }}">
-                                Elimina
-                            </button>
-                        </div>
-                    </div>
-
-                    <!-- Modal -->
-                    <div class="modal fade" id="deleteModal{{ $plate->id }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $plate->id }}" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content border-danger">
-                                <div class="modal-header border-danger">
-                                    <h1 class="modal-title fs-5 text-danger" id="deleteModalLabel{{ $plate->id }}">Elimina Piatto</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>Sei sicuro di voler eliminare il piatto <strong>{{ $plate->name }}</strong>? Questa azione sarà irreversibile!</p>
-                                </div>
-                                <div class="modal-footer border-danger">
-                                    <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">Annulla</button>
-                                    <form action="{{ route('admin.plates.destroy', $plate->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-outline-danger">Elimina</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                @endforeach
-            @else
-                <div class="alert alert-warning text-center">
-                    <h4 class="alert-heading">Non hai ancora creato nessun piatto!</h4>
-                    <p>Clicca qui sotto per creare subito un piatto e iniziare a vendere.</p>
-                    <a href="{{ route('admin.plates.create') }}" class="btn btn-primary">Crea un Piatto</a>
-                </div>
-            @endif
         </div>
     </div>
-</div>
 @endsection
-
