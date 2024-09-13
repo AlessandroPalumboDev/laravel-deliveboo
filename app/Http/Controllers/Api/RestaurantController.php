893781c8cc540fbb8047462f60b3d1907dd03851
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-09-13 16:41:04 +0200

    [UPD] Create controller e rotte per ristoranti e piatti

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use App\Models\Type;
+
+class RestaurantController extends Controller
+{
+    // Lista dei ristoranti con almeno un tipo
+    public function index(Request $request) {
+        // Recupera i ristoranti che hanno almeno un tipo
+        $query = Restaurant::with('type')->whereHas('type');
+
+        // Filtro per tipo se passato nel request
+        if ($request->has('type')) {
+            $categories = explode(',', $request->input('type'));
+            $query->whereHas('type', function ($q) use ($categories) {
+                $q->whereIn('name', $categories);
+            });
+        }
+
+        // Paginazione dei risultati
+        $restaurants = $query->paginate(6);
+
+        // Risposta JSON
+        return response()->json([
+            'success' => true,
+            'results' => $restaurants
+        ]);
+    }
+
+    // Lista dei piatti di un singolo ristorante
+    public function getPlatesByRestaurant($restaurantId) {
+        // Trova il ristorante tramite ID e carica i piatti
+        $restaurant = Restaurant::with('plates')->find($restaurantId);
+
+        // Se il ristorante non esiste, restituisci un errore
+        if (!$restaurant) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Restaurant not found',
+            ], 404);
+        }
+
+        // Restituisci la lista dei piatti del ristorante
+        return response()->json([
+            'success' => true,
+            'plates' => $restaurant->plates
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\PlateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Rotta per ottenere i dettagli dell'utente autenticato (protetta con Sanctum)
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Rotta per la lista dei ristoranti con almeno un tipo
+Route::get('restaurants', [RestaurantController::class, 'index']);
+
+// Rotta per ottenere la lista dei piatti di un singolo ristorante
+Route::get('restaurants/{restaurant}/plates', [RestaurantController::class, 'getPlatesByRestaurant']);
+
+// Rotta per mostrare un singolo piatto
+Route::get('plates/{id}', [PlateController::class, 'show']);
