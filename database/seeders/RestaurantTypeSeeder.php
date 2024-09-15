27699c40057f556da1299277e5756d298b5a5562
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-09-16 01:53:58 +0200

    [FIX] Modifica seeder tabella ResaurantType

--- database/seeders/RestaurantTypeSeeder.php
+++ database/seeders/RestaurantTypeSeeder.php
@@ -16,14 +16,72 @@ class RestaurantTypeSeeder extends Seeder
     {
         Schema::disableForeignKeyConstraints();
 
-        for($i = 0; $i < 5; $i++){
-            $new_restaurant_type = new RestaurantType();
+        // for($i = 0; $i < 5; $i++){
+        //     $new_restaurant_type = new RestaurantType();
 
-            $new_restaurant_type->restaurant_id = 1;
-            $new_restaurant_type->type_id = 3;
+        //     $new_restaurant_type->restaurant_id = 1;
+        //     $new_restaurant_type->type_id = 3;
 
-            $new_restaurant_type->save();
-        }
+        //     $new_restaurant_type->save();
+        // }
+
+        // Tipologie ristorante 1
+        RestaurantType::create([
+            'restaurant_id' => 1,
+            'type_id' => 1,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 1,
+            'type_id' => 2,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 1,
+            'type_id' => 3,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 1,
+            'type_id' => 4,
+        ]);
+
+        // Tipologie ristorante 2
+        RestaurantType::create([
+            'restaurant_id' => 2,
+            'type_id' => 4,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 2,
+            'type_id' => 5,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 2,
+            'type_id' => 6,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 2,
+            'type_id' => 7,
+        ]);
+
+        // Tipologie ristorante 3
+        RestaurantType::create([
+            'restaurant_id' => 3,
+            'type_id' => 7,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 3,
+            'type_id' => 8,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 3,
+            'type_id' => 9,
+        ]);
+        RestaurantType::create([
+            'restaurant_id' => 3,
+            'type_id' => 1,
+        ]);
+
+
+
+
 
         Schema::enableForeignKeyConstraints();
     }
